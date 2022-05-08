b8a469edf9fd76a17aecff2abe2585c5bffcc231
axel <ztylspook@gmail.com>
2022-05-08 19:37:00 -0400

    ejercicio aleatorio e info nutricional listos con ts y jquery

--- js/nutricional.js
+++ js/nutricional.js
@@ -8,30 +8,58 @@ function CalcIMC() {
         console.log(pair[1]);
         cont++;
     }
-    let peso = parseInt(selections[0]);
-    let altura = parseInt(selections[1]) / 100;
-    let aux = peso / (altura * altura);
+    var peso = parseInt(selections[0]);
+    var altura = parseInt(selections[1]) / 100;
+    var aux = peso / (altura * altura);
     var imc = Math.round(aux * 10) / 10;
     var premsg = "<p>Tu IMC es de " + imc + ", por lo tanto, ";
-    if (imc < 18.5) {
-        $(".resultado").append(premsg + "te encuentras bajo tu peso ideal...</p>");
+    if (imc < 11) {
+        $(".textresultado").html("Verifica los datos ingresados...");
+        $("#quedebobutton").prop("hidden", true);
+    }
+    if (imc >= 11 && imc < 18.5) {
+        $(".textresultado").html(premsg + "te encuentras bajo tu peso ideal...</p>");
+        $("#quedebobutton").prop("hidden", false);
     }
     if (imc >= 18.5 && imc < 25) {
-        $(".resultado").append(premsg + "te encuentras en tu peso ideal. ¡Hurra!</p>");
+        $(".textresultado").html(premsg + "te encuentras en tu peso ideal. ¡Hurra!</p>");
+        $("#quedebobutton").prop("hidden", false);
     }
     if (imc >= 25 && imc < 30) {
-        $(".resultado").append(premsg + "te encuentras sobre tu peso ideal...</p>");
+        $(".textresultado").html(premsg + "te encuentras sobre tu peso ideal...</p>");
+        $("#quedebobutton").prop("hidden", false);
     }
     if (imc >= 30 && imc < 35) {
-        $(".resultado").append(premsg + "te encuentras en Obesidad I...</p>");
+        $(".textresultado").html(premsg + "te encuentras en Obesidad I...</p>");
+        $("#quedebobutton").prop("hidden", false);
     }
     if (imc >= 35 && imc < 40) {
-        $(".resultado").append(premsg + "te encuentras en Obesidad II...</p>");
+        $(".textresultado").html(premsg + "te encuentras en Obesidad II...</p>");
+        $("#quedebobutton").prop("hidden", false);
     }
     if (imc >= 40 && imc < 50) {
-        $(".resultado").append(premsg + "te encuentras en Obesidad III...</p>");
+        $(".textresultado").html(premsg + "te encuentras en Obesidad III...</p>");
+        $("#quedebobutton").prop("hidden", false);
     }
-    if (imc >= 50) {
-        $(".resultado").append(premsg + "te encuentras en Obesidad IV...</p>");
+    if (imc >= 50 && imc < 75) {
+        $(".textresultado").html(premsg + "te encuentras en Obesidad IV...</p>");
+        $("#quedebobutton").prop("hidden", false);
+    }
+    if (imc >= 75) {
+        $(".textresultado").html("Verifica los datos ingresados...");
+        $("#quedebobutton").prop("hidden", true);
+    }
+    return imc;
+}
+function ScrollToDiet() {
+    var imc = CalcIMC();
+    if (imc >= 11 && imc < 18.5) {
+        document.getElementById("sup").scrollIntoView({ behavior: 'smooth', block: 'center', inline: 'center' });
+    }
+    if (imc >= 18.5 && imc < 25) {
+        document.getElementById("bal").scrollIntoView({ behavior: 'smooth', block: 'center', inline: 'center' });
+    }
+    if (imc >= 25) {
+        document.getElementById("def").scrollIntoView({ behavior: 'smooth', block: 'center', inline: 'center' });
     }
 }
