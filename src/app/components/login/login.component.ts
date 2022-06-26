c68d45e130b975c13596b2b8b847a8598265be81
axel <ztylspook@gmail.com>
2022-06-26 02:03:29 -0400

    register y login formularios reactivos

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import * as $ from "jquery";
 
 @Component({
   selector: 'app-login',
@@ -6,10 +8,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+  title='formLogin';
+  public formLogin!: FormGroup;
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.formLogin = this.formBuilder.group({
+      username: ['', [Validators.required]],
+      password: ['', Validators.required]
+  });
   }
 
+  mostrar(): any{
+    var usuario = this.formLogin.controls['username'].value;
+    var contra = this.formLogin.controls['password'].value;
+    alert("Usuario: "+usuario+"\nContraseña: "+contra+"\nAquí se obtienen los datos, se procesan en la API del angel");
+  }
 }
--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { CompilerFactory, Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import * as $ from "jquery";
+import { ConfirmedValidator } from '../../confirmed.validator';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +9,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
+  title='formRegister';
+  public formRegister!: FormGroup;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.formRegister = this.formBuilder.group({
+      username: ['', Validators.required],
+      email: ['', [Validators.required,Validators.email]],
+      password1: ['',Validators.required],
+      password2: ['',Validators.required]
+    }, {
+      validator: ConfirmedValidator('password1','password2')
+  })
+  }
+  mostrar(): any{
+    var usuario = this.formRegister.controls['username'].value;
+    var correo = this.formRegister.controls['email'].value;
+    var contra1 = this.formRegister.controls['password1'].value;
+    var contra2 = this.formRegister.controls['password2'].value;
+    alert("Usuario: "+usuario+"\nEmail: "+correo+"\n1era contraseña: "+contra1+"\n2da contraseña: "+contra2+"\nAquí se obtienen los datos, se procesan en la API del angel");
   }
 
 }
